c0fd6d0cb56ec47c095d6b3dc09b040382e3cff8
dillan teagle <dillanteagle@dillans-MacBook-Pro.local>
2020-09-07 15:14:32 -0500

    DAG Factory

---
+++ clients/clientOne/DAG.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.dummy_operator import DummyOperator
+
+project_name = 'test_project'
+
+default_args = {
+    "owner" : 'airflow',
+    "depends_on_past" : False,
+    "schedule_interval" : None,
+    "start_date" : "2019-05-13",
+    "email" : [ 'testaccount@dummycompany.com' ],
+    "email_on_failure" : True,
+    "email_on_retry" : False,
+    "retries" : 1,
+    "retry_delay" : timedelta(minutes=1)
+}
+
+
+main_dag = DAG(
+    project_name,
+    default_args = default_args,
+    schedule_interval= "" # get_schedule()
+)
+
+start = DummyOperator(
+    dag = main_dag,
+    task_id = project_name
+)
---
+++ dags/DAGFactory.py
@@ -0,0 +1,43 @@
+from importlib import import_module
+from airflow import DAG
+import traceback
+import sys
+import os
+
+
+AIRFLOW_BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(AIRFLOW_BASE)
+
+class DAGFactory(object):
+
+    def __init__(self):
+        self.project_base = os.path.join(AIRFLOW_BASE, 'clients')
+    
+    def projects(self):
+        return list(filter(lambda x: x not in ['__pycache__'],
+            [dirs for _, dirs in os.walk(self.project_base)][0]))
+
+    def get_modules_from_clients(self):
+        for client in self.projects:
+            try:
+                module_name = "clients.{}.DAG".format(client)
+                print(module_name)
+                prj_mod = import_module(module_name)
+                no_of_project_dags = 1
+
+                for dag in prj_mod.DAGS:
+                    var_name = '{}-{}'.format(client, no_of_project_dags)
+                    globals()[var_name] = dag
+
+                    no_of_project_dags += 1
+            except Exception as e:
+                print( traceback.format_exc() )
+
+
+
+dags = DAGFactory()
+print(dags.projects)
+
+clients = dags.get_modules_from_clients
+
+print(clients)
